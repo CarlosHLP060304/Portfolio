8a5e39402c93b7554b335521466bb5a8624636e5
Carlos Henrique Lins Ponchirolli <107359480+CarlosHLP060304@users.noreply.github.com>
2024-04-15 19:01:00 -0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+import {array_tecnologias,array_videos} from "./jsons.js";
+
 let tecnologias = document.querySelector("#tecnologias")
 const projetos = document.querySelector("#projetos") 
 const navegacao_cabecalho= document.querySelector("#cabecalho-navegacao");
@@ -7,50 +9,6 @@ let projetos_json = []
 const busca = document.querySelector("#busca")
 
 
-let src = [
-    "../img/html5.jpg",
-    "../img/css3.jpg",
-    "../img/javascript.png",
-    "../img/bootstrap.png",
-    "../img/java.png",
-    "../img/spring.jpg",
-    "../img/sql.jpg",
-    "../img/kotlin.jpg",
-    "../img/dart.jpg",
-    "../img/flutter.jpg",
-    "../img/git.png",
-    "../img/github.jpeg"
-]
-
-
-let alt= [  
-    "logo html5",
-    "logo css3",
-    "logo javascript",
-    "logo bootstrap",
-    "logo java",
-    "logo spring",
-    "logo sql",
-    "logo kotlin",
-    "logo dart",
-    "logo flutter",
-    "logo git",
-    "logo github"
-]
-let descricoes =  [
-    "Linguagem de marcação usada para criar e estruturar conteúdo na web, usando elementos e tags para definir a organização e apresentação de texto, imagens, links e outros elementos em páginas da internet.",
-    "Linguagem usada para estilizar e formatar documentos HTML, permitindo controlar o design, layout e aparência visual de elementos web, como cores, fontes, espaçamentos e posicionamentos.",
-    "Linguagem de programação versátil e amplamente usada na web, permitindo a criação de interatividade, funcionalidades dinâmicas e manipulação de elementos em páginas web, além de ser executada tanto no navegador quanto em servidores.",
-    "Framework front-end de código aberto que simplifica o desenvolvimento web, oferecendo um conjunto de componentes, estilos predefinidos e recursos responsivos para criar rapidamente interfaces modernas e bem projetadas.",
-    "Linguagem de programação versátil, orientada a objetos e amplamente utilizada, conhecida por sua portabilidade, segurança e grande comunidade de desenvolvedores. É usado em uma variedade de aplicações, desde software de desktop até aplicativos móveis e sistemas de servido",
-    "O Spring é um framework de aplicativo Java de código aberto e de propósito geral, criado para facilitar o desenvolvimento de aplicativos corporativos robustos e escaláveis. Ele fornece um ambiente abrangente e modular para o desenvolvimento de aplicativos Java, com suporte para diversas tecnologias e padrões, como injeção de dependência, controle transacional, acesso a dados, segurança, desenvolvimento de APIs RESTful e muito mais.",
-    "Linguagem de de consulta estruturada usada para gerenciar e manipular bancos de dados relacionais. Ela permite a criação, recuperação, inserção, atualização e exclusão de dados de maneira eficiente e organizada.",        
-    "Kotlin é uma linguagem de programação moderna, concisa e segura, desenvolvida pela JetBrains, que se destaca como a linguagem preferencial para o desenvolvimento de aplicativos Android. Com sintaxe limpa e expressiva, Kotlin oferece recursos poderosos, como nulidade segura e interoperabilidade perfeita com código Java, permitindo aos desenvolvedores escrever aplicativos Android robustos e eficientes.",
-    "Dart é a linguagem de programação padrão para o desenvolvimento de aplicativos Flutter, o que permite aos desenvolvedores criar interfaces de usuário ricas e responsivas para uma ampla variedade de plataformas a partir de um único código-fonte.",
-    "Flutter é um kit de desenvolvimento de software de interface de usuário (UI) de código aberto criado pela Google, utilizado para construir aplicativos compilados nativamente tanto para dispositivos Android quanto iOS, além de ser compatível com web e desktop, tudo a partir de um único código-fonte em Dart.",
-    "O Git é uma ferramenta que registra e acompanha as mudanças feitas em projetos de software. Ele permite voltar a versões anteriores, ajuda equipes a trabalhar juntas sem problemas e mantém um histórico claro de todas as alterações, o que é essencial para o desenvolvimento de software colaborativo.",
-    "O GitHub é um site para guardar projetos de software usando o Git. Permite colaboração em equipe, facilita o trabalho simultâneo, revisão de código e o registro das mudanças feitas ao longo do tempo. É uma plataforma central para desenvolvedores compartilharem e gerenciarem seus projetos de maneira eficiente."
-]
 
 let videos = [
 "videos/agendamento_medico.mp4",
@@ -80,6 +38,10 @@ function ativaBackgroundCabecalho(){
         cabecalho.classList.add("cabecalho-scroll");
     }
 }
+
+
+
+
 
 function adicionarTecnologia(id,src,alt,paragrafo){
     tecnologias_json.push({
@@ -190,18 +152,14 @@ document.getElementById("menu-sanduiche").addEventListener('click',function(e){
     console.log("acionou");
 });
 
-for (let index = 0; index < src.length; index++) {
-        adicionarTecnologia(index,src[index],alt[index],descricoes[index])
+for (let index = 0; index < array_tecnologias.length; index++) {
+        adicionarTecnologia(index,array_tecnologias[index].src,array_tecnologias[index].alt,array_tecnologias[index].paragrafo)
 }
 
-for (let index = 0; index < videos.length; index++) {
-    adicionarProjeto(index,titulos[index],videos[index])
+for (let index = 0; index < array_videos.length; index++) {
+    adicionarProjeto(index,array_videos[index].titulo,array_videos[index].video)
 }
 
-let contador_tecnologias = 2
-
-
-contador_projetos = 2
 
 
    
---
+++ js/jsons.js
@@ -0,0 +1,99 @@
+export let array_tecnologias = [
+    {
+        "id": 0,
+        "src": "../img/html5.jpg",
+        "alt": "logo html5",
+        "paragrafo": "Linguagem de marcação usada para criar e estruturar conteúdo na web, usando elementos e tags para definir a organização e apresentação de texto, imagens, links e outros elementos em páginas da internet."
+    },
+    {
+        "id": 1,
+        "src": "../img/css3.jpg",
+        "alt": "logo css3",
+        "paragrafo": "Linguagem usada para estilizar e formatar documentos HTML, permitindo controlar o design, layout e aparência visual de elementos web, como cores, fontes, espaçamentos e posicionamentos."
+    },
+    {
+        "id": 2,
+        "src": "../img/javascript.png",
+        "alt": "logo javascript",
+        "paragrafo": "Linguagem de programação versátil e amplamente usada na web, permitindo a criação de interatividade, funcionalidades dinâmicas e manipulação de elementos em páginas web, além de ser executada tanto no navegador quanto em servidores."
+    },
+    {
+        "id": 3,
+        "src": "../img/bootstrap.png",
+        "alt": "logo bootstrap",
+        "paragrafo": "Framework front-end de código aberto que simplifica o desenvolvimento web, oferecendo um conjunto de componentes, estilos predefinidos e recursos responsivos para criar rapidamente interfaces modernas e bem projetadas."
+    },
+    {
+        "id": 4,
+        "src": "../img/java.png",
+        "alt": "logo java",
+        "paragrafo": "Linguagem de programação versátil, orientada a objetos e amplamente utilizada, conhecida por sua portabilidade, segurança e grande comunidade de desenvolvedores. É usado em uma variedade de aplicações, desde software de desktop até aplicativos móveis e sistemas de servido"
+    },
+    {
+        "id": 5,
+        "src": "../img/spring.jpg",
+        "alt": "logo spring",
+        "paragrafo": "O Spring é um framework de aplicativo Java de código aberto e de propósito geral, criado para facilitar o desenvolvimento de aplicativos corporativos robustos e escaláveis. Ele fornece um ambiente abrangente e modular para o desenvolvimento de aplicativos Java, com suporte para diversas tecnologias e padrões, como injeção de dependência, controle transacional, acesso a dados, segurança, desenvolvimento de APIs RESTful e muito mais."
+    },
+    {
+        "id": 6,
+        "src": "../img/sql.jpg",
+        "alt": "logo sql",
+        "paragrafo": "Linguagem de de consulta estruturada usada para gerenciar e manipular bancos de dados relacionais. Ela permite a criação, recuperação, inserção, atualização e exclusão de dados de maneira eficiente e organizada."
+    },
+    {
+        "id": 7,
+        "src": "../img/kotlin.jpg",
+        "alt": "logo kotlin",
+        "paragrafo": "Kotlin é uma linguagem de programação moderna, concisa e segura, desenvolvida pela JetBrains, que se destaca como a linguagem preferencial para o desenvolvimento de aplicativos Android. Com sintaxe limpa e expressiva, Kotlin oferece recursos poderosos, como nulidade segura e interoperabilidade perfeita com código Java, permitindo aos desenvolvedores escrever aplicativos Android robustos e eficientes."
+    },
+    {
+        "id": 8,
+        "src": "../img/dart.jpg",
+        "alt": "logo dart",
+        "paragrafo": "Dart é a linguagem de programação padrão para o desenvolvimento de aplicativos Flutter, o que permite aos desenvolvedores criar interfaces de usuário ricas e responsivas para uma ampla variedade de plataformas a partir de um único código-fonte."
+    },
+    {
+        "id": 9,
+        "src": "../img/flutter.jpg",
+        "alt": "logo flutter",
+        "paragrafo": "Flutter é um kit de desenvolvimento de software de interface de usuário (UI) de código aberto criado pela Google, utilizado para construir aplicativos compilados nativamente tanto para dispositivos Android quanto iOS, além de ser compatível com web e desktop, tudo a partir de um único código-fonte em Dart."
+    },
+    {
+        "id": 10,
+        "src": "../img/git.png",
+        "alt": "logo git",
+        "paragrafo": "O Git é uma ferramenta que registra e acompanha as mudanças feitas em projetos de software. Ele permite voltar a versões anteriores, ajuda equipes a trabalhar juntas sem problemas e mantém um histórico claro de todas as alterações, o que é essencial para o desenvolvimento de software colaborativo."
+    },
+    {
+        "id": 11,
+        "src": "../img/github.jpeg",
+        "alt": "logo github",
+        "paragrafo": "O GitHub é um site para guardar projetos de software usando o Git. Permite colaboração em equipe, facilita o trabalho simultâneo, revisão de código e o registro das mudanças feitas ao longo do tempo. É uma plataforma central para desenvolvedores compartilharem e gerenciarem seus projetos de maneira eficiente."
+    }
+]
+
+
+export let array_videos = [
+    {
+        "id": 0,
+        "titulo": "Site para Administrador de Eventos",
+        "video": "videos/agendamento_medico.mp4"
+    },
+    {
+        "id": 1,
+        "titulo": "Montador de Currículos",
+        "video": "videos/buscador_cep_java.mp4"
+    },
+    {
+        "id": 2,
+        "titulo": "Gerador de Senha",
+        "video": "videos/gerador_senha.mp4"
+    },
+    {
+        "id": 3,
+        "titulo": "Sistema ONG Alimentando Esperanças",
+        "video": "videos/sistema_ong.mp4"
+    }
+]
+
